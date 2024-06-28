1f9c6c782911bdf6e04e39f3eed000f0e49149b2
namaek2 <scienc4588@gmail.com>
2024-06-29 03:20:47 +0900

    hyprlock update

--- hypr/hypr/scripts/LockScreen.sh
+++ hypr/hypr/scripts/LockScreen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sleep 0.1
-hyprctl keyword general:cursor_inactive_timeout 1
+#hyprctl keyword cursor:inactive_timeout 1
 pidof hyprlock || hyprlock
-hyprctl keyword general:cursor_inactive_timeout 0
+#hyprctl keyword cursor:inactive_timeout 60
