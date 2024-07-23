3818ad3483c7c6d1af102af94c4eb4058b2bf47f
namaek2 <science4588@gmail.com>
2024-07-23 09:39:17 +0900

    popup fix, rofi clipboard theme adapted, waybar fixed

--- hypr/scripts/ClipManager.sh
+++ hypr/scripts/ClipManager.sh
@@ -3,7 +3,7 @@
 # Clipboard Manager
 
 if [[ ! $(pidof rofi) ]]; then
-	cliphist list | rofi -dmenu -config ~/.config/rofi/config-long.rasi | cliphist decode | wl-copy
+	cliphist list | rofi -dmenu -config ~/.config/rofi/config-clip.rasi | cliphist decode | wl-copy
 else
 	pkill rofi
 fi
