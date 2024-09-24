52193dadb8532dc362d0bfaf91423a1ef9df483a
namaek2 <science4588@gmail.com>
2024-09-24 19:37:05 +0900

    display fixed

---
+++ hypr/hypr/scripts/LockScreen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#sleep 0.1
+#hyprctl keyword cursor:inactive_timeout 1
+pidof hyprlock || hyprlock
+sleep 0.1
+sudo dell-bios-fan-control 1
+#hyprctl keyword cursor:inactive_timeout 60
