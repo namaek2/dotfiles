b1321c34b28e2e11fa9f197c35b492088af7879c
namaek2 <science4588@gmail.com>
2025-03-05 21:51:07 +0900

    suspend fix

--- hypr/scripts/LockScreen.sh
+++ hypr/scripts/LockScreen.sh
@@ -4,5 +4,5 @@
 #hyprctl keyword cursor:inactive_timeout 1
 pidof hyprlock || hyprlock
 sleep 0.1
-sudo dell-bios-fan-control 1
+# sudo dell-bios-fan-control 1
 #hyprctl keyword cursor:inactive_timeout 60
